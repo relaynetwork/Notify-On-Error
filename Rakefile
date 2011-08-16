0b91ba7f639af1168d6edf143e845f2ab757132a
Josh and Paul <kburton@relaynetwork.com>
2011-08-16 16:06:55 -0400

    render 500 after error notification

--- Rakefile
+++ Rakefile
@@ -6,7 +6,7 @@ begin
     gem.files = Dir["{lib}/**/*", "{app}/**/*", "{config}/**/*"]
     gem.authors = ['Paul Santa Clara']
     gem.email  = 'psantaclara@relaynetwork.com'
-    gem.version = '1.0.0'
+    gem.version = '1.0.1'
   end
 rescue
   puts "Jeweler or one of its dependencies is not installed."
--- lib/application_controller.rb
+++ lib/application_controller.rb
@@ -9,19 +9,20 @@ module NotifyOnError
         Rails.logger.error( "Exception found #{ex.exception}") 
         Rails.logger.error( "Parameters #{params.inspect}") 
         Rails.logger.error( ex.backtrace.join("\n") )
-        unless NotifyOnError.notification_environments.map(&:to_s).include?( Rails.env )
-          Rails.logger.error( "Supressing notification because of environment(#{Rails.env})" )
-          return
-        end
-        
-        case NotifyOnError.notification_method
-        when :action_mailer
-          Rails.logger.error( "Notify via ActionMailer" )
-          ErrorMailer.exception_email(ex).deliver 
+        if NotifyOnError.notification_environments.map(&:to_s).include?( Rails.env )
+          case NotifyOnError.notification_method
+          when :action_mailer
+            Rails.logger.error( "Notify via ActionMailer" )
+            ErrorMailer.exception_email(ex).deliver 
+          else
+            Rails.logger.error( "Unrecognized notification method(#{NotifyOnError.notification_method})" )
+          end #case 
         else
-          Rails.logger.error( "Unrecognized notification method(#{NotifyOnError.notification_method})" )
-        end 
-      end
+          Rails.logger.error( "Suppressing notification because of environment(#{Rails.env})" )
+        end
+
+        render :status => 500
+      end #rescue
     end
 
     module InstanceMethods
